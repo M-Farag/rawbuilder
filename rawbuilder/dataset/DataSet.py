6939f0bfe9abf37871bab0490e25ba6d5616e850
M-Farag <mina.farag@icloud.com>
2021-10-24 17:27:40 +0200

    ADD: DataSet constructor

--- rawbuilder/dataset/DataSet.py
+++ rawbuilder/dataset/DataSet.py
@@ -1,3 +1,17 @@
 class DataSet:
-    def __init__(self):
-        self._schema = 'hello world'
+
+    def __init__(self, size: int, schema: list, file_name: str):
+        """
+        DataSet object constructor
+
+        Args:
+            size (int): the maximum rows size per dataset
+            schema (list): All the schemas that will build up the dataset
+            file_name (str): The dataset output filename
+
+        Returns:
+            object dataset
+        """
+        self._size = size
+        self._schema = schema
+        self._file_name = file_name
