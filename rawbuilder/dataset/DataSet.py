2e8f8b8662151289991a5d4b7a5a1bdf8a4bcc0c
M-Farag <mina.farag@icloud.com>
2021-11-04 17:39:52 +0200

    UPD: DataSet builder to validate tasks

--- rawbuilder/dataset/DataSet.py
+++ rawbuilder/dataset/DataSet.py
@@ -1,6 +1,7 @@
 import pkg_resources
 import yaml
 from yaml.loader import SafeLoader
+from ..mocker.Mocker import Mocker
 
 
 class DataSet:
@@ -51,10 +52,49 @@ class DataSet:
         Reading the schema file and init the schema  and the schema_location properties
 
         Returns:
-            boolean
+            Bool
         """
         schema_path = pkg_resources.resource_filename(__name__, "../schema.yml")
         with open(schema_path) as file:
             self._schema = yaml.load(file, Loader=SafeLoader)
             self._schema_location = schema_path
         return True
+
+    def build(self):
+        """
+        Build the dataset
+        """
+        self.__validate_tasks_exist_in_schema()
+
+        for task_name in self._tasks:
+            self.__build_task(self.schema.get(task_name))
+
+        return True
+
+    def __validate_tasks_exist_in_schema(self):
+        """
+        Validate that all the required tasks are included in the schema
+
+        Raises:
+            ValueError(Exception): If the user provided a task that is not in the schema file
+
+        Returns:
+            Bool
+        """
+
+        for task in self._tasks:
+            if task not in self.schema.keys():
+                raise ValueError('Task: {} Not found in the schema file'.format(task))
+
+        return True
+
+    def __build_task(self, task_breakdown: dict):
+        """
+        Understand and Build all the requested tasks
+
+        Args:
+            task_breakdown(dict): The task breakdown as columns and data-sources
+        Returns:
+            Bool
+        """
+        print(task_breakdown)
