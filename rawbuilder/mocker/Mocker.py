87f54cc57a4217df5d4023d70ace2b10f64fd923
M-Farag <mina.farag@icloud.com>
2021-11-05 00:05:53 +0200

    ADD: Mocker Data class <<Proof of concept>>

--- rawbuilder/mocker/Mocker.py
+++ rawbuilder/mocker/Mocker.py
@@ -1,2 +1,19 @@
 class Mocker:
-    pass
+
+    def __init__(self):
+        """
+        The Data Mocker object
+        """
+
+    def build(self, size: int, mock_type):
+        """
+
+        Args:
+            mock_type(str|list): the mock type
+            size(int): the maximum rows number
+
+        Returns:
+            list
+        """
+        return [i for i in range(size)]
+
